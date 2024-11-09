7d1a4221f6e6aae6ef4f61f38fcf347ed2cbdd5e
pulnip <choiwh1008@gmail.com>
2024-11-09 16:13:36 +0900

    :construction: fix github variable length limit problem

--- claim_daily_reward.py
+++ claim_daily_reward.py
@@ -1,4 +1,4 @@
-from genshin import Game, Client, InvalidCookies, AlreadyClaimed
+from genshin import Game
 
 class AccountInfo:
     id: str
@@ -26,43 +26,50 @@ class AccountInfo:
             elif(s=="ZZZ"):
                 self.target_games.append(Game.ZZZ)
 
-    async def claim(self):
-        client=Client(
-            lang="ko-kr",
-            cookies={
-                "ltuid_v2": self.ltuid,
-                "ltoken_v2": self.ltoken,
-                "ltmid_v2": self.ltmid
-            }
-        )
+async def claim(account: AccountInfo, lang: str="ko-kr"):
+    from genshin import Client, InvalidCookies, AlreadyClaimed
 
-        for game in self.target_games:
-            try:
-                await client.claim_daily_reward(game=game)
-                status="✅"
-            except(InvalidCookies, AlreadyClaimed) as e:
-                status="🟡"
-            
-            _, day=await client.get_reward_info(game=game)
-            rewards=await client.get_monthly_rewards(game=game)
-            reward=rewards[day-1]
+    client=Client(
+        lang=lang,
+        cookies={
+            "ltuid_v2": account.ltuid,
+            "ltoken_v2": account.ltoken,
+            "ltmid_v2": account.ltmid
+        }
+    )
 
-            print(f"User{self.id} Claimed[{status}]: {reward.name} x{reward.amount}")
+    for game in account.target_games:
+        try:
+            await client.claim_daily_reward(game=game)
+            status="✅"
+        except(InvalidCookies, AlreadyClaimed) as e:
+            status="🟡"
+        
+        _, day=await client.get_reward_info(game=game)
+        rewards=await client.get_monthly_rewards(game=game)
+        reward=rewards[day-1]
+
+        print(f"User{account.id} Claimed[{status}]: {reward.name} x{reward.amount}")
 
 if __name__ == "__main__":
     import os
     import asyncio
 
+    preferred_lang=os.getenv("PREFER_LANG")
+    if preferred_lang is None:
+        preferred_lang="en-us"
+
     account_num=int(os.getenv("HOYO_ACCOUNT_NUM"))
-    ltuids=os.getenv("USERS_LTUID").split(",")
-    ltokens=os.getenv("USERS_LTOKEN").split(",")
-    ltmids=os.getenv("USERS_LTMID").split(",")
-    users_games=os.getenv("USERS_TARGET_GAMES").split(",")
     accounts=[]
 
     for i in range(account_num):
-        accounts.append(AccountInfo(id=str(i), ltuid=ltuids[i], ltoken=ltokens[i],
-                                    ltmid=ltmids[i], games=users_games[i]))
+        accounts.append(AccountInfo(
+            id=str(i),
+            ltuid=os.getenv(f"USER{i}_LTUID"),
+            ltoken=os.getenv(f"USER{i}_LTOKEN"),
+            ltmid=os.getenv(f"USER{i}_LTMID"),
+            games=os.getenv(f"USER{i}_TARGET_GAMES")
+        ))
 
     for account in accounts:
-        asyncio.run(account.claim())
+        asyncio.run(claim(account, preferred_lang))
